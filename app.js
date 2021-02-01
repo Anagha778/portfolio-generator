61697ebe6ab3e9333da9c9cd17e30d2032178916
anagha778 <anagha778@gmail.com>
2021-01-31 22:37:57 -0500

    generated webpage

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-console.log("Hello Node!");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
+/*
 const printProfileData = (profileDataArr) => {
     profileDataArr.forEach((profileItem) => {
         console.log(profileItem)
       });
   };
   
-  printProfileData(profileDataArgs);
+  printProfileData(profileDataArgs);*/
+  const fs = require('fs');
+  const generatePage = require('./src/page-template.js');
+  
+  const profileDataArgs = process.argv.slice(2, process.argv.length);
+  const [name, github] = profileDataArgs;
+
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
